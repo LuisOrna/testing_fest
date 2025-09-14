11f78ecce913b3458fa5813465693dd64396f5ab
Luis Orna <prof.luis.orna@gmail.com>
2025-09-14 20:13:15 -0300

    Domingo en la noche

---
+++ test/test_servidor.py
@@ -0,0 +1,11 @@
+# Importar el módulo servidor
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from servidor import broadcast, clientes, nombres
+
+def test_broadcast_basico():
+    # Este será tu primer test
+    # Vamos a probarlo paso a paso
+    pass
